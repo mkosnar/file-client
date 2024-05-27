6c233bc9659b7207f22455566487f9c3b52ae5a5
mkosnar <mkosnar@seznam.cz>
2024-05-27 10:04:36 +0200

    added SSL certificate support added help descriptions

---
+++ clients/rest_client.py
@@ -0,0 +1,52 @@
+import requests
+
+from typing import Optional
+
+
+class RestClient:
+    STAT_TIMEOUT = 5
+    READ_TIMEOUT = 10
+
+    def __init__(self, base_url: str, cert_file_path: Optional[str] = None):
+        self.base_url = base_url
+        self.cert_file_path = cert_file_path
+
+    def stat(self, uuid: str) -> dict:
+        """Requests file metadata.
+
+        Args:
+            uuid: unique file identifier
+
+        Returns:
+            file metadata as json dict
+
+        Raises:
+            requests.HTTPError
+            requests.RequestException
+        """
+        resp = requests.get(f"{self.base_url}/file/{uuid}/stat",
+                            cert=self.cert_file_path,
+                            timeout=self.STAT_TIMEOUT)
+        resp.raise_for_status()
+
+        return resp.json()
+
+    def read(self, uuid: str) -> bytes:
+        """Requests file content.
+
+        Args:
+            uuid: unique file identifier
+
+        Returns:
+            file content
+
+        Raises:
+            requests.HTTPError
+            requests.RequestException
+        """
+        resp = requests.get(f"{self.base_url}/file/{uuid}/read",
+                            cert=self.cert_file_path,
+                            timeout=self.READ_TIMEOUT)
+        resp.raise_for_status()
+
+        return resp.content
